bf9a1ba999f161287b9fe929a2ae163934c5bac4
Sergio Teixeira <sergiopintoteixeira@gmail.com>
2020-02-20 12:58:58 +0100

    init

---
+++ main.tf
@@ -0,0 +1,66 @@
+resource "aws_elasticache_replication_group" "redis" {
+  replication_group_id          = "${format("%.20s", "${var.name}-${var.environment}")}"
+  replication_group_description = "Terraform managed | ${var.replication_group_description}"
+  number_cache_clusters         = var.redis_clusters
+  node_type                     = var.redis_node_type
+  automatic_failover_enabled    = var.redis_failover
+  engine_version                = var.redis_version
+  port                          = var.redis_port
+  parameter_group_name          = aws_elasticache_parameter_group.redis_parameter_group.id
+  subnet_group_name             = aws_elasticache_subnet_group.redis_subnet_group.id
+  security_group_ids            = [aws_security_group.redis_security_group.id]
+  apply_immediately             = var.apply_immediately
+  maintenance_window            = var.redis_maintenance_window
+  snapshot_window               = var.redis_snapshot_window
+  snapshot_retention_limit      = var.redis_snapshot_retention_limit
+  tags                          = "${merge(map("Name", format("terraform-elasticache-%s", var.name)), var.tags)}"
+  snapshot_name                 = var.snapshot_name
+
+}
+
+resource "aws_elasticache_parameter_group" "redis_parameter_group" {
+  name = "${replace(format("%.255s", lower(replace("redis-${var.name}-${var.environment}", "_", "-"))), "/\\s/", "-")}"
+
+  description = "Terraform-managed ElastiCache parameter group for ${var.name}-${var.environment}"
+
+  # Strip the patch version from redis_version var
+  family = "redis${replace(var.redis_version, "/\\.[\\d]+$/", "")}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_elasticache_subnet_group" "redis_subnet_group" {
+  name       = "${replace(format("%.255s", lower(replace("redis-${var.name}-${var.environment}", "_", "-"))), "/\\s/", "-")}"
+  subnet_ids = var.subnets
+}
+
+resource "aws_security_group" "redis_security_group" {
+  name        = "${format("%.255s", "${var.name}-${var.environment}")}"
+  description = "Terraform-managed ElastiCache security group for ${var.name}-${var.environment}"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "${var.name}-${var.environment}"
+  }
+}
+
+resource "aws_security_group_rule" "redis_ingress" {
+  count                    = "${length(var.allowed_security_groups)}"
+  type                     = "ingress"
+  from_port                = var.redis_port
+  to_port                  = var.redis_port
+  protocol                 = "tcp"
+  source_security_group_id = "${element(var.allowed_security_groups, count.index)}"
+  security_group_id        = aws_security_group.redis_security_group.id
+}
+
+resource "aws_security_group_rule" "redis_networks_ingress" {
+  type              = "ingress"
+  from_port         = var.redis_port
+  to_port           = var.redis_port
+  protocol          = "tcp"
+  cidr_blocks       = var.allowed_cidr
+  security_group_id = aws_security_group.redis_security_group.id
+}
---
+++ variables.tf
@@ -0,0 +1,83 @@
+variable "apply_immediately" {
+  description = "Specifies whether any modifications are applied immediately, or during the next maintenance window. Default is false."
+  default     = "false"
+}
+
+variable "allowed_cidr" {
+  type        = list
+  default     = ["127.0.0.1/32"]
+  description = "A list of Security Group ID's to allow access to."
+}
+
+variable "allowed_security_groups" {
+  type        = list
+  default     = []
+  description = "A list of Security Group ID's to allow access to."
+}
+
+variable "environment" {
+  description = "env to deploy into, should typically dev/staging/prod"
+}
+
+variable "name" {
+  description = "Name for the Redis replication group i.e. UserObject"
+}
+
+variable "redis_clusters" {
+  description = "Number of Redis cache clusters (nodes) to create"
+}
+
+variable "redis_failover" {
+  default = false
+}
+
+variable "redis_node_type" {
+  description = "Instance type to use for creating the Redis cache clusters"
+  default     = "cache.m3.medium"
+}
+
+variable "redis_port" {
+  default = 6379
+}
+
+variable "subnets" {
+  type        = list
+  description = "List of VPC Subnet IDs for the cache subnet group"
+}
+
+# might want a map
+variable "redis_version" {
+  description = "Redis version to use, defaults to 3.2.10"
+  default     = "3.2.10"
+}
+
+variable "snapshot_name" {
+  default = ""
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+}
+
+variable "redis_maintenance_window" {
+  description = "Specifies the weekly time range for when maintenance on the cache cluster is performed. The format is ddd:hh24:mi-ddd:hh24:mi (24H Clock UTC). The minimum maintenance window is a 60 minute period"
+  default     = "fri:08:00-fri:09:00"
+}
+
+variable "redis_snapshot_window" {
+  description = "The daily time range (in UTC) during which ElastiCache will begin taking a daily snapshot of your cache cluster. The minimum snapshot window is a 60 minute period"
+  default     = "06:30-07:30"
+}
+
+variable "redis_snapshot_retention_limit" {
+  description = "The number of days for which ElastiCache will retain automatic cache cluster snapshots before deleting them. For example, if you set SnapshotRetentionLimit to 5, then a snapshot that was taken today will be retained for 5 days before being deleted. If the value of SnapshotRetentionLimit is set to zero (0), backups are turned off. Please note that setting a snapshot_retention_limit is not supported on cache.t1.micro or cache.t2.* cache nodes"
+  default     = 0
+}
+
+variable "tags" {
+  type        = map
+  description = "Tags for redis nodes"
+  default     = {}
+}
+
+variable "replication_group_description" {}
